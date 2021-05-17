7e1a543377709c0264823531ca293c4cb7e6efe5
Isaías Mordente <39633055+yipisaias@users.noreply.github.com>
2021-05-17 01:09:41 -0300

    Major adjusts concerning controls and style

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useRef } from 'react';
 import { Button } from 'antd';
 import { CaretRightFilled, PauseOutlined } from '@ant-design/icons';
@@ -6,20 +5,23 @@ import Cronometro from '../components/Cronometro';
 import colors from '../utils/colors';
 import styles from './Home.module.css';
 
+let cronoRunning = null;
+
 function Home() {
   const crono1Ref = useRef(null);
   const crono2Ref = useRef(null);
-  const bp1 = useRef(null);
-  const bp2 = useRef(null);
-  const [cronoRunning, setCronoRunning] = useState(null);
+  const p1BtnRef = useRef(null);
+  const p2BtnRef = useRef(null);
+  const [cronoRunningBtn, setCronoRunningBtn] = useState(null);
   const [play, setPlay] = useState(true);
 
   const iconStyle = {
-    color: `${colors.default}`,
+    color: colors.default,
     fontSize: '3em',
+    display: 'block',
   };
 
-  const buttonStyle = {
+  const playersButtonStyle = {
     backgroundColor: colors.primary,
     border: '4px solid red',
     width: '85%',
@@ -28,6 +30,13 @@ function Home() {
     margin: 'auto',
     fontSize: '5em',
     cursor: 'auto',
+  };
+
+  const controlsButtonStyle = {
+    height: '90%',
+    padding: 0,
+    backgroundColor: colors.secondary,
+    borderColor: colors.secondary,
   };
 
   const bindCronos = (crono1, crono2) => {
@@ -39,35 +48,61 @@ function Home() {
     }
   };
 
-  const pause = () => {
-    if (crono1Ref.current.state.stop && crono2Ref.current.state.stop) {
-      cronoRunning.pararTempo();
-    } else if (crono1Ref.current.state.stop) {
-      crono2Ref.current.pararTempo();
-      setCronoRunning(crono2Ref.current);
-    } else {
-      crono1Ref.current.pararTempo();
-      setCronoRunning(crono1Ref.current);
+  const pauseUnpause = () => {
+    if (!crono1Ref.current.state.stop || !crono2Ref.current.state.stop) {
+      if (!crono1Ref.current.state.stop) {
+        cronoRunning = crono1Ref.current;
+        setCronoRunningBtn(p1BtnRef.current);
+      } else if (!crono2Ref.current.state.stop) {
+        cronoRunning = crono2Ref.current;
+        setCronoRunningBtn(p2BtnRef.current);
+      }
     }
+    cronoRunning.pararTempo();
     setPlay(!play);
   };
 
   useEffect(() => {
-    // bp1.current.disabled = crono1Ref.current.state.stop;
-    bp1.current.onclick = () => {
+    p1BtnRef.current.onclick = () => {
+      if (crono1Ref.current.state.stop) {
+        p2BtnRef.current.disabled = true;
+      } else {
+        p1BtnRef.current.disabled = true;
+        p2BtnRef.current.disabled = false;
+      }
       bindCronos(crono1Ref.current, crono2Ref.current);
     };
 
-    // bp2.current.disabled = crono2Ref.current.state.stop;
-    bp2.current.onclick = () => {
+    p2BtnRef.current.onclick = () => {
+      if (crono2Ref.current.state.stop) {
+        p1BtnRef.current.disabled = true;
+      } else {
+        p2BtnRef.current.disabled = true;
+        p1BtnRef.current.disabled = false;
+      }
       bindCronos(crono2Ref.current, crono1Ref.current);
     };
-  }, [bp1, bp2]);
+  }, [p1BtnRef, p2BtnRef]);
+
+  useEffect(() => {
+    if (cronoRunningBtn !== null) {
+      if (!play) {
+        cronoRunningBtn.disabled = true;
+      } else {
+        cronoRunningBtn.disabled = false;
+      }
+    }
+  }, [play]);
 
   return (
     <div className={styles.Container}>
-      <Button type="primary" shape="circle" style={buttonStyle} ref={bp1}>
-        <Cronometro id="p1" ref={crono1Ref} />
+      <Button
+        type="primary"
+        shape="circle"
+        style={playersButtonStyle}
+        ref={p1BtnRef}
+      >
+        <Cronometro ref={crono1Ref} />
       </Button>
 
       <div
@@ -80,15 +115,21 @@ function Home() {
           justifyContent: 'space-around',
         }}
       >
-        {React.createElement(play ? PauseOutlined : CaretRightFilled, {
-          style: iconStyle,
-          onClick: () => pause(),
-        })}
+        <Button style={controlsButtonStyle} onClick={pauseUnpause}>
+          {React.createElement(play ? PauseOutlined : CaretRightFilled, {
+            style: iconStyle,
+          })}
+        </Button>
         <div className={styles.StopIcon}>&nbsp;</div>
       </div>
 
-      <Button type="primary" shape="circle" style={buttonStyle} ref={bp2}>
-        <Cronometro id="p2" ref={crono2Ref} />
+      <Button
+        type="primary"
+        shape="circle"
+        style={playersButtonStyle}
+        ref={p2BtnRef}
+      >
+        <Cronometro ref={crono2Ref} />
       </Button>
     </div>
   );
